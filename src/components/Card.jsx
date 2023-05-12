fbcdd9a25a30ea1477e56b40b38e0bba99460a6f
nayanrdeveloper <nayanrdeveloper@gmail.com>
2023-05-12 11:42:31 +0530

    feat: dark-mode added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,7 +18,7 @@ export default function Card() {
         return (
           <div
             key={i}
-            className="mx-auto bg-[#8800ff]  w-5/6 flex-col flex p-6 items-center rounded-lg shadow-2xl text-white relative md:hover:shadow-3xl mb-12 md:w-11/12 md:flex-row  md:gap-24  md:p-12"
+            className="mx-auto bg-[#8800ff]  w-5/6 flex-col flex p-6 items-center rounded-lg shadow-2xl text-white relative md:hover:shadow-3xl mb-12 md:w-11/12 md:flex-row  md:gap-24  md:p-12 dark:bg-[#242435]"
           >
             {/* image */}
             <a href={item.link}>
@@ -32,25 +32,25 @@ export default function Card() {
             {/* Content */}
             <div className="content  flex flex-col  py-4 pr-4 md:p-2 ">
               <a href={item.link}>
-                <h2 className="text-sm text-purple-200  mb-6 tracking-[0.1rem] font-bold hover:text-[white] md:text-3xl">
+                <h2 className="text-sm text-purple-200  mb-6 tracking-[0.1rem] font-bold hover:text-[white] dark:hover:text-[#00a3ff] md:text-3xl dark:text-white">
                   {item.title}
                 </h2>
               </a>
-              <p className="text-xs  font-inter md:w-5/6 md:text-lg">
+              <p className="text-xs  font-inter md:w-5/6 md:text-lg dark:text-[#acacac]">
                 {item.description}
               </p>
             </div>
             {/* Links */}
             <div
-              className="flex  absolute bg-purple-500  gap-2 md:gap-12 justify-between 
+              className="flex  absolute bg-purple-500 dark:bg-[#00a3ff]  gap-2 md:gap-12 justify-between 
             flex-col right-0 top-0 h-full w-6 rounded-tr-lg rounded-br-lg  px-4 py-6 
              md:p-4 items-center md:h-full md:py-12 md:px-4 md:w-12 md:right-0 md:flex-col "
             >
               <a href={item.ghLink} target="_blank" rel="noopener noreferrer">
-                <BiGitRepoForked className=" text-[#062a64] hover:text-white md:text-3xl" />
+                <BiGitRepoForked className=" text-[#062a64] dark:text-white hover:text-white md:text-3xl" />
               </a>
               <a href={item.link} target="_blank" rel="noopener noreferrer">
-                <BsFillArrowUpRightCircleFill className=" text-[#062a64] hover:text-white md:text-3xl" />
+                <BsFillArrowUpRightCircleFill className=" text-[#062a64] dark:text-white hover:text-white md:text-3xl" />
               </a>
             </div>
           </div>
